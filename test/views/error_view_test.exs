1ae386f3f1457305dd873980fe296214865423c9
Ville Heikkinen <vheikkinen@topcon.com>
2016-04-20 16:55:40 +0300

    Fix error view tests

--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -1,21 +1,23 @@
 defmodule ReactiveServer.ErrorViewTest do
   use ReactiveServer.ConnCase, async: true
+  
+  @moduletag :error_view_case
 
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
-  test "renders 404.html" do
-    assert render_to_string(ReactiveServer.ErrorView, "404.html", []) ==
-           "Page not found"
+  test "renders 404.html", %{conn: conn} do
+    assert render_to_string(ReactiveServer.ErrorView, "404.html", [conn: conn]) 
+    |> String.contains?("Sorry, the page you are looking for does not exist.")
   end
 
-  test "render 500.html" do
-    assert render_to_string(ReactiveServer.ErrorView, "500.html", []) ==
-           "Server internal error"
+  test "render 500.html", %{conn: conn} do
+    assert render_to_string(ReactiveServer.ErrorView, "500.html", [conn: conn])
+    |> String.contains?("Server internal error")
   end
 
-  test "render any other" do
-    assert render_to_string(ReactiveServer.ErrorView, "505.html", []) ==
-           "Server internal error"
+  test "render any other", %{conn: conn} do
+    assert render_to_string(ReactiveServer.ErrorView, "505.html", [conn: conn])
+    |> String.contains?("Server internal error")
   end
 end
